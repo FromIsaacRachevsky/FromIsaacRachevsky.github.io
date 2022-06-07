0c5425142250628abf21b4eebc64093d92f95280
FromIsaacRachevsky <107083441+FromIsaacRachevsky@users.noreply.github.com>
2022-06-08 01:21:27 +0200

    Add files via upload

---
+++ maison d'hotes compressé/inscriptionFR/index.js
@@ -0,0 +1,5 @@
+document.querySelector("h2").style.background = "black";
+
+const questionContainer = document.querySelector(".type_escort");
+
+document.querySelector(".type_escort").style.background = "purple";
---
+++ maison d'hotes compressé/site en Anglais/test1.php
@@ -0,0 +1,146 @@
+
+    
+    </head>
+    <body>
+        <header>
+
+        <?php
+          include("menu.php")
+        ?>
+            <nav>
+                <ul>
+                    <li id="logo"><a href="#">My Modèle - Offres lingeries</a></li>
+                    <li><a href="#langue">Langue</a></li>
+                    <li><a href="#reserver">Réserver</a></li>
+                    <li><a href="#contact">Nous contacter</a></li>
+                    <li><a href="Modèles annonces">Modèles annonces</a></li>
+                    <li><a href="Nos modèles">Nos modèles</a></li>
+                    <li><a href="exo2.php">exo2</a></li>
+                    <li><a href="test1.php">test1</a></li>
+                    <li><a href="test2.php">test2</a></li>
+                    <li id="Flags">
+                        <div id="languages">
+
+                            <a href="#imageFR" title="FR"><img src="https://st.sexemodel.com/img/flags/france.png" alt="FR" width="30" height="18"></a> 
+                            <a href="imageEN" title="EN"><img src="https://st.sexemodel.com/img/flags/united kingdom.png" alt="EN" width="30" height="18"></a>
+
+                        </div>
+
+                        
+                    </li>
+                    
+                </ul>
+            </nav>
+            <nav id="languebarre">
+                <ul>
+                    <li id="languages">
+                        
+                        
+                    
+
+                    
+                </ul>
+               
+
+                </li>
+
+            </nav>
+            <div id="imagePrincipale">
+                <h1>My modèle</h1>
+                <div id="premierTrait"></div>
+                <h3>Offres lingeries</h3>
+            </div>
+            <div class="Boutton Cliquez">
+                <h3> Cliquez ici</h3>
+            </div>
+
+
+        </header>
+        <section id="presentation">
+            <div id="texteIntro">
+                <h2>Ce qui se cahe derrière My Modèle</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil porro ratione sit mollitia fugit temporibus
+                   voluptatibus. Labore illo totam iure molestiae accusamus, odio vitae natus libero beatae quaerat sint dignissimos 
+                   magni voluptatibus ad alias eum minus sunt cumque deserunt commodi excepturi recusandae amet! Dolores, quam maxime? 
+                   Atque consequatur voluptatum quasi, in eveniet animi quo! Ut suscipit ad autem quis quibusdam consequatur non cum. 
+                   Dolorum expedita provident non nam, amet aut ab vel temporibus repellat dolore sit sunt. Veniam, incidunt facilis! </p>
+            </div>
+            <div id="prestations">
+                <div class="imagesPrestations">
+                    <h4>Lingeries proposées</h4>
+                    <a href="#"><img src="media/dessin lingerie.png" alt="dessin lingerie"></a>
+                </div>
+                <div class="imagesPrestations">
+                    <h4>Nos modèles</h4>
+                    <a href="#"><img src="media/nos modèles image.jpg" alt="image lingerie rouge"></a>
+                </div>
+                <div class="imagesPrestations">
+                    <h4>Les prestations</h4>
+                    <a href="#"><img src="media/Les prestations.jpg" alt="image string"></a>
+                </div>
+            </div>
+
+
+
+        </section>
+        <section id="Description">
+          <h2>Ce qui se cache derrière My Modèle</h2>
+          <ul>
+            <li id="ocean"><p>L'océan atlantique</p></li>
+            <li id="chateau"><p>Le chateau du Médoc</p></li>
+            <li id="phare"><p>L'estuaire de la Gironde</p></li>
+            <li id="vignoble"><p>Le vignoble Bordelais</p></li>
+          </ul>
+        </section>
+        <footer>
+            <h2 id="contact">Contactez-nous</h2>
+            <form>
+                <input placeholder="Nom">
+                <input placeholder="E-mail">
+                <textarea placeholder="Votre message ici..."></textarea>
+                <button>Envoyer</button>
+            </form>
+            <div id="deuxièmeTrait"></div>
+            <div id="copyrightEtIcons">
+                <div id="copyright">
+                    <span>© fromIsaacRachevsky(); 2022</span>
+                </div>
+                <div id="icons">
+                    <a href="http://www.twitter.fr"><i class="fa fa-twitter"></i></a>
+                    <a href="http://www.facebook.fr"><i class="fa fa-facebook"></i></a>
+                    <a href="http://www.instagram.com"><i class="fa fa-instagram"></i></a>
+
+                </div>
+            </div>
+      </footer>
+    </body>
+
+    <popu id="">
+        Attention: Le site internet MY Modèle est reserve e un public majeur
+Avertissement
+Cette partie du site est un service réservé à un public majeur et averti. Ce service peut contenir des textes et des photos qui peuvent être choquants pour certaines sensibilités.
+
+Je certifie sur l'honneur :
+- être majeur selon la loi en vigueur dans mon pays de résidence,
+- être informé du caractère pour adultes de cette partie du site,
+- que mon pays de résidence m'autorise à consulter ce service,
+- consulter ce service à titre personnel sans impliquer de quelque manière que ce soit une société privée ou un organisme public.
+
+Je m'engage sur l'honneur à :
+- ne pas faire état de l'existence de ce service et à ne pas en diffuser le contenu à des mineurs,
+- utiliser tous les moyens permettant d'empécher l'accès de ce service à tout mineur,
+- ne pas poursuivre la société éditrice de toute action judiciaire,
+- assumer ma responsabilité si un mineur accède à ce service à cause de ma négligence,
+- assumer ma responsabilité si une ou plusieurs de mes présentes déclarations sont inexactes.
+
+Censure et respect des lois internationales
+Toute annonce ne respectant pas les législations internationales sera immédiatement censurée par nos modérateurs. Les services sexuels tarifés sont strictement interdits. Le site SexeModel ne peut en aucun cas être tenu pour responsable du contenu des annonces. En cas de plainte, l'éditeur de SexeModel se réserve le droit de transmettre vos coordonnées numériques et autres éventuelles informations aux autorités compétentes. Pour plus d'informations, nous vous invitons à consulter nos conditions générales d'utilisation.
+
+Protection des mineurs et filtre parental
+Aux parents soucieux que leur enfant soit facilement confronté à du contenu pornographique sur Internet, nous vous recommandons l'installation de filtres. Faites une recherche sur les termes « contrôle parental » à partir de votre moteur de recherche habituel.
+Je certifie être d'accord avec les règles qui précèdent et je signe électroniquement mon accord :
+ANNULEROK
+    </popu>
+    <p></p>
+
+</html>
